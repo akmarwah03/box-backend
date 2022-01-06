052ba04f9961c387040ce031d164dc2b1738c369
Akshay <amarwah4@my.bcit.ca>
2022-01-06 13:41:17 -0800

    Fixed cors

--- app.js
+++ app.js
@@ -6,16 +6,14 @@ const cors = require("cors");
 const authRouter = require("./routes/auth");
 
 const app = express();
-
-app.use(express.json());
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
-app.use(cors());
+
+app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
